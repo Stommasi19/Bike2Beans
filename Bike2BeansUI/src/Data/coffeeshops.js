5e850923536e79c8c8bde26dd5140819768f79c0
Stommasi19 <161857751+Stommasi19@users.noreply.github.com>
2026-02-13 21:30:04 -0800

    update to style (#12)
    * update to style
    
    * user rating total now scalar, fixed import

--- Bike2BeansUI/src/Data/coffeeshops.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.coffeeShops = void 0;
-exports.coffeeShops = [
-    { id: "1", name: "Victrola Coffee", rating: 4.6, lat: 47.6145, lng: -122.3191 },
-    { id: "2", name: "Anchorhead Coffee", rating: 4.5, lat: 47.6163, lng: -122.3355 },
-    { id: "3", name: "Espresso Vivace", rating: 4.7, lat: 47.6236, lng: -122.3382 },
-];
